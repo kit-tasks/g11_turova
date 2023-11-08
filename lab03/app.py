52b31c3e30a6afdd577491bc3b2a7577df12a5a3
allinois <92531328+allinois@users.noreply.github.com>
2023-11-08 09:03:05 +0300

    3 and 4 works are ready for evaluation 3

--- lab03/app.py
+++ lab03/app.py
@@ -1,8 +1,23 @@
 from flask import Flask, request
 
-app = Flask('__name__')
+app = Flask(__name__)
 
-@app.route('/HelloName', methods = ['GET'])
-def my_form():
+@app.route('/HelloNameGet', methods = ['GET'])
+def HelloNameGet():
     value = request.args.get('param')
+    doc = ''
+    doc += '<!doctype html>'
+    doc += '<html lang = "ru">'
+    doc += '<head>'
+    doc += '</head>'
+    doc += '<body>'
+    doc += '<h1> result with GET method </h1>'
+    doc += '<hr>'
+    doc += 'hello, ' + value + '!'
+    doc += '</body>'
+    doc += '</html>'
+    return doc
+
+
+
     
